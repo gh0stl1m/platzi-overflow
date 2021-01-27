3484d07e9a09b1d67d476deba4f3f59d22725dba
Santiago Sanchez Taborda <santiago.sanchez.t@gmail.com>
2021-01-27 17:12:58 -0500

    fix(User): Add validation to register contreoller

--- src/deliver/http/routes/users/index.js
+++ src/deliver/http/routes/users/index.js
@@ -1,5 +1,4 @@
-const Joi = require('@hapi/joi');
-const Joi = require('@hapi/joi');
+const Joi = require('joi');
 
 const { users } = require('../../../../adapters');
 
@@ -8,11 +7,11 @@ module.exports = [
     method: 'POST',
     options: {
       validate: {
-        payload: {
+        payload: Joi.object({
           name: Joi.string().required().min(3),
           email: Joi.string().email().required(),
           password: Joi.string().required().min(6),
-        }
+        })
       }
     },
     path: '/api/users',
